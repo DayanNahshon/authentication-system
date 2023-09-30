0b625f38d4fbfb7722432162e448824da804d464
Dayan <dayannbryan1@gmail.com>
2023-09-30 13:09:07 +0300

    Update index.ts

--- auth/src/pages/index.tsx
+++ auth/src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const text1: string =
     "This is a full build that covers the full authentification process from login, Register, sending emails (for activating acount,reset password and password change notice), forgot password, reset password,advanced form validation, protected routes, session manipulation...";
   const text2: string =
-    "In this build we used React js, Next js, Mongodb, Mongoose, Typecript, Next auth,React-hook-form, Zod, Axios, Nodemailer, Smtp service, Gmail stmp, Axios, React-toastify, Zxcvbn, Handlebars, BcryptJs...";
+    "In this build we used React js, Next js, Mongodb, Mongoose, Typecript, Next auth, React-hook-form, Smtp service, Gmail stmp, Axios, React-toastify, Handlebars, BcryptJs...";
 
   return (
       <div className="home bg-black min-h-screen text-white flex items-center justify-center">
