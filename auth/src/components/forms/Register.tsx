9377937791980d5e7d14572360dc0acd12aac1e0
Dayan <dayannbryan1@gmail.com>
2023-10-01 13:20:34 +0300

    Update

--- auth/src/components/forms/Register.tsx
+++ auth/src/components/forms/Register.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react'
 import Input from '../inputs/Input'
-import {CiUser} from "react-icons/ci"
-import { SubmitHandler, useForm } from "react-hook-form"
+import { CiUser } from "react-icons/ci"
+import { FiMail, FiLock } from "react-icons/fi"
+import { BsTelephone } from "react-icons/bs"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import validator from "validator"
+import Link from 'next/link'
 
 
-//-----Form Schema
+//-----Form Schema (with Zod)
 const FormSchema = z.object({
     first_name: z.string()
         .min(2, "First name must be atleast 2 characters")
         .max(32, "First name must be less than 32 characters")
         .regex(new RegExp("^[a-zA-z]+$"), "No special characters such as:<>/%#&?', are allowed"),
+    last_name: z.string()
+        .min(2, "Last name must be atleast 2 characters")
+        .max(32, "Last name must be less than 32 characters")
+        .regex(new RegExp("^[a-zA-z]+$"), "No special characters such as:<>/%#&?', are allowed"),
+    email: z.string().email("Please enter a valid email address"),
+    phone: z.string().refine(validator.isMobilePhone, { message: "Please enter a valid phone number"}),
+    password: z.string()
+        .min(8, "Your password must be at least 8 characters")
+        .max(52, "Your password must be less than 52 characters"),
 })
 
 //-----Type Form Schema
@@ -24,23 +37,69 @@ const RegisterForm: React.FunctionComponent = () => {
     console.log(watch())
 
     return (
-        <form className='my-8 text-sm' onSubmit={handleSubmit(onSubmit)}>
-            <div className='gap-2 md:flex'>
-                <Input
-                    name="first_name"
-                    label="First name"
-                    type="text"
-                    icon={<CiUser/>}
-                    placeholder="Enter Your First Name"
-                    register={register}
-                    error={errors?.first_name?.message}
-                    disabled={isSubmitting}
-                />
+        <div className="w-full px-12 py-4">
+            <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">Sign Up</h2>
+            <p className="text-center text-sm text-gray-600 mt-2">
+                You already have an account? &nbsp;
+                <Link href="/auth" className="text-blue-600 hover:text-blue-700 hover:underline cursor-pointer">Sign In</Link>
+            </p>
+            <form className='my-8 text-sm' onSubmit={handleSubmit(onSubmit)}>
+                <div className="gap-2 md:flex">
+                    <Input
+                        name="first_name"
+                        label="First name"
+                        type="text"
+                        icon={<CiUser/>}
+                        placeholder="Enter Your First Name"
+                        register={register}
+                        error={errors?.first_name?.message}
+                        disabled={isSubmitting}
+                    />
+                    <Input
+                        name="last_name"
+                        label="Last name"
+                        type="text"
+                        icon={<CiUser/>}
+                        placeholder="Enter Your Last Name"
+                        register={register}
+                        error={errors?.last_name?.message}
+                        disabled={isSubmitting}
+                    />
+                </div>
+                    <Input
+                        name="email"
+                        label="Email"
+                        type="email"
+                        icon={<FiMail/>}
+                        placeholder="Enter Your Email"
+                        register={register}
+                        error={errors?.email?.message}
+                        disabled={isSubmitting}
+                    />
+                    <Input
+                        name="phone"
+                        label="Phone Number"
+                        type="text"
+                        icon={<BsTelephone/>}
+                        placeholder="Enter Your Phone Number"
+                        register={register}
+                        error={errors?.phone?.message}
+                        disabled={isSubmitting}
+                    />
+                    <Input
+                        name="password"
+                        label="Password"
+                        type="password"
+                        icon={<FiLock/>}
+                        placeholder="Enter Your Password"
+                        register={register}
+                        error={errors?.password?.message}
+                        disabled={isSubmitting}
+                    />
                 <button type='submit'>Submit</button>
-            </div>
-        </form>
+            </form>
+        </div>
     ) 
-     
 }
 
 export default RegisterForm
--- auth/src/pages/auth.tsx
+++ auth/src/pages/auth.tsx
@@ -1,20 +1,12 @@
 import RegisterForm from "@/components/forms/Register"
-import { signIn } from "next-auth/react";
 
 export default function auth() {
     return (
         <div className="w-full flex items-center justify-center">
             <div className="w-full h-100 flex items-center justify-center">
+                {/*----Form----*/}
                 <div className="w-full sm:w5/6 md:w-2/3 lg:w1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex flex-col items-center justify-center">
-                    <div className="w-full px-12 py-4">
-                        <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">Sign Up</h2>
-                        <p className="text-center text-sm text-gray-600 mt-2">
-                            You already have an account? &nbsp;
-                            <a className="text-blue-600 hover:text-blue-700 hover:underline cursor-pointer" onClick={()=>signIn()}>Sign In</a>
-                        </p>
-                        {/* SIGN UP FORM */}
-                        <RegisterForm/>
-                    </div>
+                    <RegisterForm/>
                 </div>
             </div>
         </div>
