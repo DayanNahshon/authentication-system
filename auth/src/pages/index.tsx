9912ecc5e7d1e2171e78ac504bcaf404cf947bc1
Dayan <dayannbryan1@gmail.com>
2023-10-01 09:35:24 +0300

    Update

--- auth/src/pages/index.tsx
+++ auth/src/pages/index.tsx
@@ -30,36 +30,33 @@ export default function Home() {
                   { 
                     session ? (
                     <button onClick={()=>signOut()} className="bg-blue-500 hover:bg-blue-700 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150">Sign Out</button>
-                    ) : ( <button onClick={()=>signIn()} className="bg-blue-500 hover:bg-blue-700 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150">Sign In</button> 
+                    ) : (
+                    <div className="w-full flex justify-center">
+                      <button onClick={()=>signIn()} className=" text-center mt-12 bg-blue-500 hover:bg-blue-700 text-xl uppercase font-bold px-20 py-5 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150">Sign In</button> 
+                    </div> 
                     ) 
                   }
-                  {/* <button
-                    className="bg-blue-500 hover:bg-blue-700 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                    onClick={() => signOut()}>Log out
-                  </button> */}
                 </div>
               </div>
-              <div className="w-full flex justify-center">
-              <img
-                src={session?.user?.image!}
-                alt={`${session?.user?.name} image`}
-                className="rounded-full h-40 w-40"
-              />
-            </div>
-            <div className="text-center mt-12">
-              <h3 className="text-4xl font-semibold mb-2">
-                {session?.user?.name}
-              </h3>
-              <div className="text-sm mb-2 font-bold">
-                {session?.user?.email}
-              </div>
-              <div className="mb-2 mt-10">
-                You logged in using &nbsp;
-                <span className="capitalize bg-blue-400 text-white px-4 py-1 ml-2 font-bold italix text-lg rounded-md">
-                  {session?.user?.provider}
-                </span>
-              </div>
-            </div>
+              {
+                session ? (
+                  <>
+                    <div className="w-full flex justify-center">
+                      <img src={session?.user?.image!} alt={`${session?.user?.name} image`} className="rounded-full h-40 w-40" />
+                    </div>
+                    <div className="text-center mt-12">
+                      <h3 className="text-4xl font-semibold mb-2">{session?.user?.name}</h3>
+                      <div className="text-sm mb-2 font-bold">{session?.user?.email}</div>
+                      <div className="mb-2 mt-10">You logged in using &nbsp;
+                        <span className="capitalize bg-blue-400 text-white px-4 py-1 ml-2 font-bold italix text-lg rounded-md">{session?.user?.provider}</span>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <div className="w-full flex justify-center"></div>
+                )
+              }
+              
             <div className="mt-10 py-10 border-t text-center">
               <div className="flex flex-wrap justify-center">
                 <div className="w-full px-4">
