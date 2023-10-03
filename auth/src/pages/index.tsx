1ea9e45b0e9ed5b6852938ce8cde852b1be58f2a
Dayan <dayannbryan1@gmail.com>
2023-10-03 09:52:06 +0300

    Update

--- auth/src/pages/index.tsx
+++ auth/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPageContext } from "next"
-import { useSession, signIn, signOut, getSession } from "next-auth/react"
+import { useSession, signOut, getSession } from "next-auth/react"
 import { AiFillGithub } from "react-icons/ai"
 import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa"
 
@@ -26,38 +26,23 @@ export default function Home() {
             <div className="flex flex-wrap justify-center items-center">
               <div className="w-full text-right">
                 <div className="py-6 px-3">
-                  { 
-                    session ? (
-                    <button onClick={()=>signOut()} className="bg-blue-500 hover:bg-blue-700 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150">Sign Out</button>
-                    ) : (
-                    <div className="w-full flex justify-center">
-                      <button onClick={()=>signIn()} className="text-center mt-12 bg-blue-500 hover:bg-blue-700 text-xl uppercase font-bold px-20 py-5 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150">Sign In</button> 
-                    </div> 
-                    ) 
-                  }
+                    <button onClick={()=>signOut()} className="bg-blue-500 hover:bg-blue-700 text-md uppercase font-bold px-8 py-2 rounded-md sm:mr-2 mb-1 ease-linear transition-all duration-150">
+                      Sign Out
+                    </button>
                 </div>
               </div>
-              {
-                session ? (
-                  <>
-                    <div className="w-full flex justify-center">
-                      <img src={session?.user?.image!} alt={`${session?.user?.name} image`} className="rounded-full h-40 w-40" />
-                    </div>
-                    <div className="text-center mt-12">
-                      <h3 className="text-4xl font-semibold mb-2">{session?.user?.name}</h3>
-                      <div className="text-sm mb-2 font-bold">{session?.user?.email}</div>
-                      <div className="mb-2 mt-10">You logged in using &nbsp;
-                      <span className="capitalize bg-blue-400 text-white px-4 py-1 ml-2 font-bold italix text-lg rounded-md">
-                        {session?.user?.provider}
-                        </span>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <div className="w-full flex justify-center"></div>
-                )
-              }
-              
+              <div className="w-full flex justify-center">
+                <img src={session?.user?.image!} alt={`${session?.user?.name} image`} className="rounded-full h-40 w-40" />
+              </div>
+              <div className="text-center mt-12">
+                <h3 className="text-4xl font-semibold mb-2">{session?.user?.name}</h3>
+                <div className="text-sm mb-2 font-bold">{session?.user?.email}</div>
+                <div className="mb-2 mt-10">You logged in using &nbsp;
+                  <span className="capitalize bg-blue-400 text-white px-4 py-1 ml-2 font-bold italix text-lg rounded-md">
+                    {session?.user?.provider}
+                  </span>
+                </div>
+              </div>
             <div className="mt-10 py-10 border-t text-center">
               <div className="flex flex-wrap justify-center">
                 <div className="w-full px-4">
